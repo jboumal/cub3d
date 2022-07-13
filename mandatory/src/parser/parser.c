77fbd5e5ffeb8ff3798cca1e349a8e1fadb3fd6d
belmoussaoui <emb-12@outlook.com>
2022-07-13 17:02:26 +0200

    basic minimap

--- mandatory/src/parser/parser.c
+++ mandatory/src/parser/parser.c
@@ -3,17 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
+/*   By: bel-mous <bel-mous@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 16:33:38 by bel-mous          #+#    #+#             */
-/*   Updated: 2022/07/13 12:59:57 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/13 17:00:52 by bel-mous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+// Temporary function to be deleted when parsing works.
+void	temp_init_map(t_game *game)
+{
+	static int	map[49] = {
+		1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 1, 0, 1,
+		1, 0, 1, 0, 0, 0, 1,
+		1, 0, 0, 0, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 1,
+		1, 0, 0, 1, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1,
+	};
+
+	game->map.data = map;
+	game->map.width = 7;
+	game->map.height = 7;
+	game->player.pos.x = 2.6;
+	game->player.pos.y = 3.4;
+}
+
 void	parser(t_game *game, char **argv)
 {
-	(void) game;
 	(void) argv;
+	temp_init_map(game);
 }
--- mandatory/src/render/render.c
+++ mandatory/src/render/render.c
@@ -3,23 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
+/*   By: bel-mous <bel-mous@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:06:51 by bel-mous          #+#    #+#             */
-/*   Updated: 2022/07/13 13:33:11 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/13 16:48:57 by bel-mous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "render.h"
 
 void	render(t_game *game)
 {
-	t_data	img;
-
-	img = get_new_img(game);
-	//
-	my_mlx_pixel_put(&img, 5, 5, 0x00FFFFFF);
-	//
-	mlx_put_image_to_window(game->mlx, game->window, img.img, 0, 0);
-	mlx_destroy_image(game->mlx, img.img);
+	mlx_clear_window(game->mlx, game->window);
+	render_minimap(game);
 }
