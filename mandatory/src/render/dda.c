ebf841756b0610e7b03bfa1173a0e102d7b328c6
valentinRog <valentin.rogister@gmail.com>
2022-07-13 15:27:34 +0200

    yo

--- mandatory/src/render/dda.c
+++ mandatory/src/render/dda.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 13:01:48 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/07/13 13:59:52 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/13 15:24:34 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,20 @@ t_vector
 	else
 		side_dist.x = (cell % game->map.width + 1.0 - pos.x) * delta_dist.x;
 	if (ray_dir.y < 0)
-		side_dist.y = (pos.y - cell % game->map.width) * delta_dist.y;
+		side_dist.y = (pos.y - cell / game->map.width) * delta_dist.y;
 	else
 		side_dist.y = (cell / game->map.width + 1.0 - pos.y) * delta_dist.y;
 	return (side_dist);
 }
 
-double	dda(t_vector ray_dir, t_game *game)
+double	dda(t_vector ray_dir, int *side, t_game *game)
 {
 	int			cell;
 	t_vector	delta_dist;
 	t_vector	side_dist;
-	int			step_x;
-	int			step_y;
 
 	cell = (int)game->player.pos.y * game->map.width + (int)game->player.pos.x;
-	delta_dist = vector(1 / ray_dir.x, 1 / ray_dir.y);
+	delta_dist = vector(1 / fabs(ray_dir.x), 1 / fabs(ray_dir.y));
 	side_dist = get_side_dist(ray_dir, cell, delta_dist, game);
 	while (!game->map.data[cell])
 	{
@@ -47,14 +45,16 @@ double	dda(t_vector ray_dir, t_game *game)
 		{
 			side_dist.x += delta_dist.x;
 			cell += -2 * (ray_dir.x < 0) + 1;
+			*side = 0;
 		}
 		else
 		{
 			side_dist.y += delta_dist.y;
 			cell += -2 * game->map.width * (ray_dir.x < 0) + game->map.width;
+			*side = 1;
 		}
 	}
-	if (side_dist.x < side_dist.y)
+	if (!*side)
 		return (side_dist.x - delta_dist.x);
 	return (side_dist.y - delta_dist.y);
 }
