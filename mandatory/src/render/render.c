da18f0d812b9c15e74ef078623a1364d6a6a0383
valentinRog <valentin.rogister@gmail.com>
2022-07-13 18:31:16 +0200

    yo

--- mandatory/src/render/render.c
+++ mandatory/src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:06:51 by bel-mous          #+#    #+#             */
-/*   Updated: 2022/07/13 17:46:59 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/07/13 18:31:07 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,7 @@
 void	render(t_game *game)
 {
 	t_data	img;
-	// //
-	// int map[] = {1,1,1,1,1,
-	// 			1,0,0,0,1,
-	// 			1,0,0,0,1,
-	// 			1,0,0,0,1,
-	// 			1,1,1,1,1};
-	// game->map.data = map;
-	// game->map.width = 5;
-	// game->map.height = 5;
-	// game->player.pos = vector(3, 3);
-	// game->player.dir = vector(-1, 0);
-	// game->player.plane = vector(0, 0.66);
-	// //
+
 	img = get_new_img(game);
 	raycasting(0, &img, game);
 	mlx_put_image_to_window(game->mlx, game->window, img.img, 0, 0);
