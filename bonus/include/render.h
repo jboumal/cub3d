078c0d7ddb65c01f1bc56781c12434f5361a3c33
valentinRog <valentin.rogister@gmail.com>
2022-08-12 00:44:07 +0200

    fixed all data races

--- bonus/include/render.h
+++ bonus/include/render.h
@@ -6,7 +6,7 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:19:48 by bel-mous          #+#    #+#             */
-/*   Updated: 2022/08/11 23:14:36 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/08/12 00:42:36 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,6 +86,10 @@ void	raycasting(int x0, int x1, t_data *img, t_game *game);
 /* render */
 void	render(t_game *game);
 void	load_floor_texture(t_game *game);
+
+/* routines */
+void	*routine1(void *arg);
+void	*routine2(void *arg);
 
 /* skycasting */
 void	load_sky_texture(t_game *game);
--- bonus/src/render/render.c
+++ bonus/src/render/render.c
@@ -6,47 +6,13 @@
 /*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:06:51 by bel-mous          #+#    #+#             */
-/*   Updated: 2022/08/11 23:17:49 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/08/12 00:41:57 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
-static int	get_start(int i, int start, int end)
-{
-	return (i * ((end - start) / N_THREAD) + start);
-}
-
-static int	get_end(int i, int start, int end)
-{
-	int	res;
-
-	res = (i + 1) * ((end - start) / N_THREAD) + start - 1;
-	if (end - res <= (end - start) / N_THREAD)
-		res += end - res;
-	return (res);
-}
-
-static void	*routine(void *arg)
-{
-	t_game	*g;
-	int		i;
-	int		start;
-	int		end;
-
-	g = ((t_th_arg *)arg)->game;
-	i = ((t_th_arg *)arg)->i;
-	start = get_start(i, SCREEN_H / 2, SCREEN_H - 1);
-	end = get_end(i, SCREEN_H / 2, SCREEN_H - 1);
-	floorcasting(start, end, ((t_th_arg *)arg)->img, g);
-	start = get_start(i, 0, SCREEN_W - 1);
-	end = get_end(i, 0, SCREEN_W - 1);
-	skycasting(start, end, ((t_th_arg *)arg)->img, g);
-	raycasting(start, end, ((t_th_arg *)arg)->img, g);
-	return (arg);
-}
-
-static void	compute_pixels(t_data *img, t_game *g)
+static void	compute_pixels(t_data *img, t_game *g, void *(*routine)(void *))
 {
 	pthread_t	th[N_THREAD];
 	int			i;
@@ -76,7 +42,8 @@ void	render(t_game *game)
 	t_data		img;
 
 	img = get_new_img(game);
-	compute_pixels(&img, game);
+	compute_pixels(&img, game, routine1);
+	compute_pixels(&img, game, routine2);
 	render_minimap(&img, game);
 	mlx_put_image_to_window(game->mlx, game->window, img.img, 0, 0);
 	mlx_destroy_image(game->mlx, img.img);
