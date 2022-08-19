c6f74574f16b15f8140a14c43abd788ba856c76b
Benjamin Perraudin <bperraud@student.s19.be>
2022-08-20 00:57:12 +0200

    object behind wall

--- bonus/include/parsing.h
+++ bonus/include/parsing.h
@@ -66,6 +66,7 @@ typedef struct s_game
 	t_texture	floor;
 	t_texture	sky;
 	t_object	list_object[10];
+	double		depth_buffer[SCREEN_W + 1];
 }	t_game;
 
 /* check_map */
--- bonus/src/render/object.c
+++ bonus/src/render/object.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 19:35:19 by bperraud          #+#    #+#             */
-/*   Updated: 2022/08/19 23:42:14 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/08/20 00:55:25 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ static void	load_object_t(t_game *game, int index, char *path_to_texture)
 	int			endian;
 	void		*img;
 
-	img = mlx_xpm_file_to_image(game->mlx, path_to_texture,
-			&game->list_object[index].obj_t.width, &game->list_object[index].obj_t.height);
+	img = mlx_xpm_file_to_image(game->mlx, path_to_texture, &game->
+		list_object[index].obj_t.width, &game->list_object[index].obj_t.height);
 	if (!img)
 		parsing_error("error when loading object texture");
 	game->list_object[index].obj_t.img = (unsigned int *)mlx_get_data_addr(img,
@@ -45,42 +45,40 @@ void	init_object(t_game *game)
 	load_object_t(game, 0, "img/object/tree.xpm");
 }
 
-static void draw_object(void *img, double fObjectCeiling, double fMiddleOfObject)
+static void	draw_object(t_game *game, void *img, t_object obj, double f_object_ceiling, double f_object_angle, double dist)
 {
+	int		lx;
+	int		ly;
+	double	f_object_height;
+	double	f_object_width;
+	int		color;
+	int		n_object_column;
 
-}
-
-static void	init_draw_object(void *img, t_object obj, double f_object_angle, double dist)
-{
-	double fObjectCeiling;
-	double fObjectHeight;
-	double fObjectWidth;
-	double fMiddleOfObject;
-	double lx;
-	double ly;
-
-	fObjectCeiling = (((double) SCREEN_H / 2.0) - ((double) SCREEN_H / ((double) dist / obj.size)));
-	fObjectHeight = (double) SCREEN_H - 2.0 * fObjectCeiling;
-	fObjectWidth = fObjectHeight / ((double) obj.obj_t.height  / (double) obj.obj_t.width);
-	fMiddleOfObject = (0.5 * (f_object_angle / (FOV / 2.0)) + 0.5) * (double) SCREEN_W;
-
-	for (lx = 0; lx < fObjectWidth; lx++)
+	f_object_height = (double) SCREEN_H - 2.0 * f_object_ceiling;
+	f_object_width = f_object_height / ((double) obj.obj_t.height / (double) obj.obj_t.width);
+	lx = 0;
+	while (lx++ < f_object_width)
 	{
-		for (ly = 0; ly < fObjectHeight; ly++)
+		ly = 0;
+		while (ly++ < f_object_height)
 		{
-			int tex = ((int) (ly / fObjectHeight * obj.obj_t.height)
-			 * obj.obj_t.height + (int) (lx / fObjectWidth * obj.obj_t.width));
-
-			int color = obj.obj_t.img[tex];
-			int nObjectColumn = (fMiddleOfObject + lx - (fObjectWidth / 2.0));
-
-			if (nObjectColumn >= 0 && nObjectColumn < SCREEN_W)
+			color = obj.obj_t.img[((int)(ly / f_object_height * obj.obj_t.height) * obj.obj_t.height + (int)(lx / f_object_width * obj.obj_t.width))];
+			n_object_column = ((0.5 * (f_object_angle / (FOV / 2.0)) + 0.5) * (double) SCREEN_W + lx - (f_object_width / 2.0));
+			if (n_object_column >= 0 && n_object_column < SCREEN_W)
 			{
-				if (color > 0)
-					my_mlx_pixel_put(img, nObjectColumn, (int) fObjectCeiling + ly, color);
+				if (color > 0 && game->depth_buffer[n_object_column] >= dist)
+					my_mlx_pixel_put(img, n_object_column, (int) f_object_ceiling + ly, color);
 			}
 		}
 	}
+}
+
+static void	init_draw_object(t_game *g, void *img, t_object obj, double f_obj_angle, double dist)
+{
+	double f_object_ceiling;
+
+	f_object_ceiling = (((double) SCREEN_H / 2.0) - ((double) SCREEN_H / ((double) dist / obj.size)));
+	draw_object(g, img, obj, f_object_ceiling, f_obj_angle, dist);
 }
 
 void	render_objects(void	*img, t_game *g)
@@ -89,27 +87,24 @@ void	render_objects(void	*img, t_game *g)
 	int			i;
 	double		fvec_x;
 	double		fvec_y;
-	double		dist;
-	double		fObjectAngle;
+	double		f_obj_angle;
 
 	i = 0;
 	while (i < 1)
 	{
 		obj = g->list_object[i];
-
 		fvec_x = obj.x - g->player.pos.x;
 		fvec_y = obj.y - g->player.pos.y;
-		dist = sqrt(fvec_x * fvec_x + fvec_y * fvec_y);
-
-		fObjectAngle = atan2(fvec_x, fvec_y) - atan2(g->player.dir.x , g->player.dir.y);
-		if (fObjectAngle < -M_PI)
-			fObjectAngle += 2.0 * M_PI;
-		if (fObjectAngle > M_PI)
-			fObjectAngle -= 2.0 * M_PI;
-
-		if (fabs(fObjectAngle) < FOV / 2.0 && sqrt(fvec_x * fvec_x + fvec_y * fvec_y) >= 1.0)
+		f_obj_angle = atan2(fvec_x, fvec_y)
+			- atan2(g->player.dir.x, g->player.dir.y);
+		if (f_obj_angle < -M_PI)
+			f_obj_angle += 2.0 * M_PI;
+		if (f_obj_angle > M_PI)
+			f_obj_angle -= 2.0 * M_PI;
+		if (fabs(f_obj_angle) < FOV / 2.0 && sqrt(fvec_x * fvec_x + fvec_y * fvec_y) >= 1.0)
 		{
-			init_draw_object(img, obj, fObjectAngle, dist);
+			init_draw_object(g, img, obj, f_obj_angle,
+				sqrt(fvec_x * fvec_x + fvec_y * fvec_y));
 		}
 		i++;
 	}
