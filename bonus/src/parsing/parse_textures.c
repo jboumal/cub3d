a04cb24b6bd6a0a1dad73d852a370a294209097b
valentinRog <valentin.rogister@gmail.com>
2022-09-07 00:43:56 +0200

    fixed leaked

--- bonus/src/parsing/parse_textures.c
+++ bonus/src/parsing/parse_textures.c
@@ -14,38 +14,42 @@
 
 void	parse_textures(t_game *g, int fd)
 {
-	t_dy_str	line;
+	char		*line;
+	t_dy_str	s;
 	t_node		*node;
 	char		**arr;
 	int			i;
 
 	while (g->map.ceil == -1)
 	{
-		line = dy_str_new();
-		dy_str_append_str(&line, get_next_non_empty_line(fd));
-		while (ft_strchr(" \t\n\r", line.str[line.len - 1]))
-			dy_str_pop_back(&line);
-		if (isascii_48(line.str[0]) && line.str[0] != 'F' && line.str[0] != 'C')
+		s = dy_str_new();
+		line = get_next_non_empty_line(fd);
+		dy_str_append_str(&s, line);
+		free(line);
+		while (ft_strchr(" \t\n\r", s.str[s.len - 1]))
+			dy_str_pop_back(&s);
+		if (isascii_48(s.str[0]) && s.str[0] != 'F' && s.str[0] != 'C')
 		{
-			arr = ft_split(line.str, ',');
+			arr = ft_split(s.str, ',');
 			i = 0;
 			while (arr[i])
 			{
 				node = new_node(x_calloc(1, sizeof(t_texture)));
 				load_texture(g->mlx, skip_spaces(arr[i] + 2), node->content);
-				clst_add_back(&g->textures[line.str[0] - 49], node);
+				clst_add_back(&g->textures[s.str[0] - 49], node);
 				i++;
 			}
+			str_arr_free(arr);
 		}
-		else if (!str_n_cmp("F ", line.str, 2))
-			load_texture(g->mlx, skip_spaces(line.str + 2), &g->floor);
-		else if (!str_n_cmp("C ", line.str, 2))
+		else if (!str_n_cmp("F ", s.str, 2))
+			load_texture(g->mlx, skip_spaces(s.str + 2), &g->floor);
+		else if (!str_n_cmp("C ", s.str, 2))
 		{
-			load_texture(g->mlx, skip_spaces(line.str + 2), &g->sky);
+			load_texture(g->mlx, skip_spaces(s.str + 2), &g->sky);
 			g->map.ceil = 1;
 		}
 		else
 			exit_error("invalid identifier");
-		dy_str_destroy(&line);
+		dy_str_destroy(&s);
 	}
 }
