c7050ff448cee48e0b27a7546717caac11bfa063
valentinRog <valentin.rogister@gmail.com>
2022-09-28 18:00:22 +0200

    update enemy pos

--- bonus/src/data_struct/vector_norme.c
+++ bonus/src/data_struct/vector_norme.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector_norme.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vrogiste <vrogiste@student.s19.be>         +#+  +:+       +#+        */
+/*   By: vrogiste <vrogiste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 11:19:22 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/08/29 11:19:53 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/09/28 17:41:13 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 double	vector_norme(t_vector v)
 {
 	return (sqrt(v.x * v.x + v.y * v.y));
+}
+
+t_vector	vector_normalize(t_vector v)
+{
+	return (vector(v.x / vector_norme(v), v.y / vector_norme(v)));
 }
--- bonus/src/gameloop/update_enemy.c
+++ bonus/src/gameloop/update_enemy.c
@@ -6,11 +6,29 @@
 /*   By: vrogiste <vrogiste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:57:03 by vrogiste          #+#    #+#             */
-/*   Updated: 2022/09/28 16:30:06 by vrogiste         ###   ########.fr       */
+/*   Updated: 2022/09/28 17:56:05 by vrogiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void update_enemy_pos(t_game *g, t_enemy *enemy)
+{
+	t_vector	v;
+	t_vector	dir;
+	t_vector	np;
+
+	v = vector(g->player.pos.x - enemy->s.x, g->player.pos.y - enemy->s.y);
+	dir = vector_normalize(v);
+	if (vector_norme(v) > 2)
+	{
+		np = vector(enemy->s.x + dir.x * (MOVE_SPEED/1000), enemy->s.y + dir.y * (MOVE_SPEED/1000));
+		if (!g->map.data[(int)np.y * g->map.width + (int)np.x])
+			enemy->s.x = np.x;
+		if (!g->map.data[(int)np.y * g->map.width + (int)np.x])
+			enemy->s.y = np.y;
+	}
+}
 
 void	update_enemy(t_game *g)
 {
@@ -28,6 +46,7 @@ void	update_enemy(t_game *g)
 				enemy = g->list_enemy[i];
 				enemy->s.image++;
 				enemy->s.image = (int)enemy->s.image % (enemy->s.t.width / enemy->s.t.height);
+				update_enemy_pos(g, enemy);
 			}
 			i++;
 		}
