ff61bc27acbed362c0fff814860450e318e85625
jboumal <jboumal@student.s19.be>
2022-09-29 18:25:13 +0200

    fix walking into doors

--- bonus/src/gameloop/update_cam.c
+++ bonus/src/gameloop/update_cam.c
@@ -19,9 +19,11 @@ static void	update_pos(t_game *g, t_vector *pos, t_vector *np)
 		|| can_pass_door((int)pos->y * g->map.width + (int)np->x, g))
 	{
 		if ((np->x - (int)np->x < 0.9999
-				|| !g->map.data[(int)pos->y * g->map.width + (int)np->x + 1])
+				|| !g->map.data[(int)pos->y * g->map.width + (int)np->x + 1]
+				|| g->map.data[(int)pos->y * g->map.width + (int)np->x + 1] == 10)
 			&& (np->x - (int)np->x > 0.0001
-				|| !g->map.data[(int)pos->y * g->map.width + (int)np->x - 1]))
+				|| !g->map.data[(int)pos->y * g->map.width + (int)np->x - 1]
+				|| g->map.data[(int)pos->y * g->map.width + (int)np->x - 1] == 10))
 			pos->x = np->x;
 	}
 	if ((!g->map.data[(int)np->y * g->map.width + (int)pos->x]
@@ -29,9 +31,11 @@ static void	update_pos(t_game *g, t_vector *pos, t_vector *np)
 		|| can_pass_door((int)np->y * g->map.width + (int)pos->x, g))
 	{
 		if ((np->y - (int)np->y < 0.9999
-				|| !g->map.data[((int)np->y + 1) * g->map.width + (int)pos->x])
+				|| !g->map.data[((int)np->y + 1) * g->map.width + (int)pos->x]
+				|| g->map.data[((int)np->y + 1) * g->map.width + (int)pos->x] == 10)
 			&& (np->y - (int)np->y > 0.0001
-				|| !g->map.data[((int)np->y - 1) * g->map.width + (int)pos->x]))
+				|| !g->map.data[((int)np->y - 1) * g->map.width + (int)pos->x]
+				|| g->map.data[((int)np->y - 1) * g->map.width + (int)pos->x] == 10))
 			pos->y = np->y;
 	}
 }
